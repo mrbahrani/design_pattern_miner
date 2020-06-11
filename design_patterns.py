c515bbaa4607e5ce060742835e2761f82a433418
Aryan Ebrahimpour <oxaryan@outlook.com>
2020-06-11 17:45:09 +0430

    Add state pattern

--- design_patterns.py
+++ design_patterns.py
@@ -3,6 +3,10 @@ import networkx as nx
 mediator_pattern = DiGraph()
 mediator_pattern.add_nodes_from([ (1, 2, {'type': "as"}), (3, 2, {'type': "as"}), (4, 1, {'type': "as"}), (4, 3, {'type': "as"}), (4, 2, {'type': "ge"}) ])
 
+state_pattern = DiGraph()
+state_pattern.add_nodes_from([ (1, 2, {'type': "as"}), (3, 2, {'type': "ge"}) ])
+
+
 design_patterns = dict()
 
 design_patterns["abstract_factory"] = nx.DiGraph()
